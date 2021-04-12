632b8b455fc6f2c24e1842d88c93eb9b87816c16
Artur Simon <artur_simon@hotmail.com>
2021-04-12 19:02:31 -0300

    Work on the algoritm for a more elegant and funcional solution

--- AspnetCore_GraphQL/Calculations/NumbersCalc.cs
+++ AspnetCore_GraphQL/Calculations/NumbersCalc.cs
@@ -7,27 +7,38 @@ namespace AspnetCore_GraphQL.Services
 {
     public static class NumbersCalc
     {
+        private static bool found = false;
+        private static int[] _match = new int[]{};
+        private static int[] _range;
         public static int[] Calculate(int[] range, int target)
         {
-            //Conform array
             range = range.Where(c => c > 0).ToArray()
                 .Distinct().ToArray();
             Array.Sort(range);
-            //decompose target with range's values
-            List<int> result = new List<int>();
-            for (int x = range.Length - 1; x >= 0; x--)
+            _range = range;
+            _match = new int[]{};
+            found = false;
+            CalcRecursive(target, _range.Length - 1, new int[0]);
+
+            return _match;
+        }
+
+        static void CalcRecursive(int target, int denom, int[] coins)
+        {
+            if (found) return;
+            if (target == 0)
             {
-                while (target / range[x] > 0)
-                {
-                    target -= range[x];
-                    result.Add(range[x]);
-                }
+                found = true;
+                _match = coins;
+                return;
             }
-            if (target > 0)
-                return new int[] { -1 };
-            int[] returnSorted = result.ToArray();
-            Array.Sort(returnSorted);
-            return returnSorted;
+            if (target < 0 || (denom < 0 && target >= 1)) return;
+            int[] coinsAdded = new int[coins.Length + 1];
+            if (coins.Length > 0)
+                Array.Copy(coins, coinsAdded, coins.Length);
+            coinsAdded[coinsAdded.Length - 1] = _range[denom];
+            CalcRecursive(target - _range[denom], denom, coinsAdded);
+            CalcRecursive(target, denom - 1, coins);
         }
 
     }
