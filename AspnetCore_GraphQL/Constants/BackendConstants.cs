be21268bf14ab5cb692752d72a6863af53aae4ac
Artur Simon <artur_simon@hotmail.com>
2021-04-17 03:25:08 -0300

    Working on client side

---
+++ AspnetCore_GraphQL/Constants/BackendConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCore_GraphQL.Constants
+{
+    public static class BackendConstants
+    {
+#if DEBUG
+        public static string GraphQLApiUrl { get; } = "https://localhost:44362/api";
+#else
+        public static string GraphQLApiUrl { get; } =  "https://audaces-backend-test.herokuapp.com/api";
+#error Missing GraphQL Api Url
+        public static string GraphQLApiUrl { get; } = "";
+#endif
+    }
+}
---
+++ AspnetCore_GraphQL/Models/ListViewModel.cs
@@ -0,0 +1,40 @@
+﻿using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using GraphQL.Language.AST;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCore_GraphQL.Models
+{
+    public static class ListViewModel
+    {
+        public static async Task<string> ExecuteOperation(int[] range, int target)
+        {
+            string returnString;
+            try
+            {
+                var client = new GraphQLHttpClient(new GraphQLHttpClientOptions
+                {
+                    EndPoint = new Uri(Constants.BackendConstants.GraphQLApiUrl)
+                }, new NewtonsoftJsonSerializer());
+                var requestString = "mutation ($input : CreateOperationInput!) {combination(input : $input)  { }}";
+                var request = new GraphQLHttpRequest
+                {
+                    Query = requestString,
+                    Variables = new Queries.CreateOperationInput { Target = target, Range = range },
+                    //variavel nao passa pra input
+                };
+                var response = await client.SendMutationAsync<string>(request);
+                returnString = response.ToString();
+            }
+            catch (Exception)
+            {
+                returnString = "Erro.";
+            }
+            return returnString;
+        }
+    }
+}
